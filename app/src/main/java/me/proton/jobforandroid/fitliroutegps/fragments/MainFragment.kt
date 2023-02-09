7d75ef62ebd0675bd54a0473b708a3d22db6a56e
Yurik Galyaminsky <jobforandroid@proton.me>
2023-02-09 08:17:21 +0600

    #3 Add Fragment

---
+++ app/src/main/java/me/proton/jobforandroid/fitliroutegps/fragments/MainFragment.kt
@@ -0,0 +1,28 @@
+package me.proton.jobforandroid.fitliroutegps.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import me.proton.jobforandroid.fitliroutegps.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+
+
+}
---
+++ app/src/main/java/me/proton/jobforandroid/fitliroutegps/fragments/TracksFragment.kt
@@ -0,0 +1,26 @@
+package me.proton.jobforandroid.fitliroutegps.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import me.proton.jobforandroid.fitliroutegps.databinding.FragmentTracksBinding
+
+class TracksFragment : Fragment() {
+
+    private lateinit var binding: FragmentTracksBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentTracksBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = TracksFragment()
+    }
+}
