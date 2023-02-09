ff83cdce82d7ef229589b0382a7a667563d7a2c1
galyaminsky <jobforandroid@proton.me>
2023-02-09 07:23:42 +0600

    #1 BottomNav

--- app/src/main/java/me/proton/jobforandroid/fitliroutegps/MainActivity.kt
+++ app/src/main/java/me/proton/jobforandroid/fitliroutegps/MainActivity.kt
@@ -2,6 +2,7 @@ package me.proton.jobforandroid.fitliroutegps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import me.proton.jobforandroid.fitliroutegps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        onBottomNavClicks()
 
+    }
+
+    private fun onBottomNavClicks() {
+        binding.bNan.setOnItemSelectedListener {
+            when(it.itemId) {
+
+                R.id.home_menu -> {
+                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                }
+                R.id.trucks_menu -> {
+                    Toast.makeText(this, "Trucks", Toast.LENGTH_SHORT).show()
+                }
+                R.id.setting_menu -> {
+                    Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
     }
 }
