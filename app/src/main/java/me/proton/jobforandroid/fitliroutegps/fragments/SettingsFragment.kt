34aad8bb3c5096e49187d4b053044a40596d81f2
Yurik Galyaminsky <jobforandroid@proton.me>
2023-02-09 21:19:35 +0600

    #6 SettingsFragment

--- app/src/main/java/me/proton/jobforandroid/fitliroutegps/fragments/SettingsFragment.kt
+++ app/src/main/java/me/proton/jobforandroid/fitliroutegps/fragments/SettingsFragment.kt
@@ -1,12 +1,36 @@
 package me.proton.jobforandroid.fitliroutegps.fragments
 
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import me.proton.jobforandroid.fitliroutegps.R
+import me.proton.jobforandroid.fitliroutegps.utils.showToast
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
+    private lateinit var timePref: Preference
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.main_preference, rootKey)
+        init()
+    }
+
+    private fun init() {
+        timePref = findPreference("update_time_key")!!
+        val changeListener = onChangeListener()
+        timePref.onPreferenceChangeListener = changeListener
+    }
+
+    private fun onChangeListener(): Preference.OnPreferenceChangeListener {
+        return Preference.OnPreferenceChangeListener {
+            pref, value ->
+            val nameArray = resources.getStringArray(R.array.loc_time_update_name)
+            val valueArray = resources.getStringArray(R.array.loc_time_update_value)
+            val title = pref.title.toString().substringBefore(":")
+            pref.title = "$title: ${nameArray[valueArray.indexOf(value)]}"
+            true
+        }
+
     }
 
 }
