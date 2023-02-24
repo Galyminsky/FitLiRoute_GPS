03b9df5b1d7e4ce1e7d50759d60e7fd5c73c1ad3
Yurik Galyaminsky <jobforandroid@proton.me>
2023-02-24 17:02:06 +0600

    #3 Room_DB (TrackItem @Entity)

---
+++ app/src/main/java/me/proton/jobforandroid/fitliroutegps/db/TrackItem.kt
@@ -0,0 +1,28 @@
+package me.proton.jobforandroid.fitliroutegps.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity (tableName = "track")
+data class TrackItem(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+
+    @ColumnInfo (name = "time")
+    val time: String,
+
+    @ColumnInfo (name = "date")
+    val date: String,
+
+    @ColumnInfo (name = "distance")
+    val distance: String,
+
+    @ColumnInfo (name = "velocity")
+    val velocity: String,
+
+    @ColumnInfo (name = "geo_points")
+    val geoPoints: String,
+
+)
