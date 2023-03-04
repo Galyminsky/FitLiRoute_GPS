ca611a9e287fe65e471ddea402b890c3a41e20f7
Yurik Galyaminsky <jobforandroid@proton.me>
2023-03-04 06:15:39 +0600

    #4 Review (Marker.icon)

--- app/src/main/java/me/proton/jobforandroid/fitliroutegps/fragments/ViewTrackFragment.kt
+++ app/src/main/java/me/proton/jobforandroid/fitliroutegps/fragments/ViewTrackFragment.kt
@@ -6,14 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import me.proton.jobforandroid.fitliroutegps.MainApp
+import me.proton.jobforandroid.fitliroutegps.R
 import me.proton.jobforandroid.fitliroutegps.databinding.FragmentViewTrackBinding
 import me.proton.jobforandroid.fitliroutegps.viewmodel.MainViewModel
 import org.osmdroid.config.Configuration
 import org.osmdroid.library.BuildConfig
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.Polyline
 
 class ViewTrackFragment : Fragment() {
@@ -49,14 +52,27 @@ class ViewTrackFragment : Fragment() {
 
             val polyline = getPolyline(it.geoPoints)
             map.overlays.add(polyline)
+            setMarkers(polyline.actualPoints)
             goToStartPosition(polyline.actualPoints[0])
         }
     }
 
     private fun goToStartPosition(startPosition: GeoPoint) {
-        binding.map.controller.zoomTo(20.0)
+        binding.map.controller.zoomTo(18.0)
         binding.map.controller.animateTo(startPosition)
+    }
 
+    private fun setMarkers(list: List<GeoPoint>) = with(binding) {
+        val startMarker = Marker(map)
+        val finishMarker = Marker(map)
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        finishMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        startMarker.icon = getDrawable(requireContext(), R.drawable.ic_loc_start)
+        finishMarker.icon = getDrawable(requireContext(), R.drawable.ic_loc_finish)
+        startMarker.position = list[0]
+        finishMarker.position = list[list.size - 1]
+        map.overlays.add(startMarker)
+        map.overlays.add(finishMarker)
     }
 
     private fun getPolyline(geoPoints: String): Polyline {
